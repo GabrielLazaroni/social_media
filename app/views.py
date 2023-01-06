2fe72b7b6acc4a10379d91b834e674bdf126a5ed
gabriel <gabriellazaroni@outlook.com.br>
2023-01-05 22:42:33 -0300

    fornt stuffs

--- app/views.py
+++ app/views.py
@@ -4,14 +4,29 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     
+    user_following_list = []
+    feed = []
+    
+    user_following = FollowersCount.objects.filter(follower=request.user.username)
+    
+    for users in user_following:
+        user_following_list.append(users.user)
+        
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+        
+    feed_list = list(chain(*feed))
+    
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
 
 @login_required(login_url='signin')
 def like_post(request):
@@ -27,12 +42,12 @@ def like_post(request):
         new_like.save()
         post.no_of_likes = post.no_of_likes + 1
         post.save()
-        return redirect('/')
+        return redirect('/index')
     else:
         like_filter.delete()
         post.no_of_likes = post.no_of_likes - 1
         post.save()
-        return redirect('/') 
+        return redirect('/index') 
 
 @login_required(login_url='signin')
 def profile(request, pk):
@@ -40,11 +55,26 @@ def profile(request, pk):
     user_profile = Profile.objects.get(user=user_object)
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
+    
+    follower = request.user.username
+    user = pk
+    
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+        
+    user_followers = len(FollowersCount.objects.filter(user=pk))    
+    user_following = len(FollowersCount.objects.filter(follower=pk))    
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
-        'user_post_length': user_post_length
+        'user_post_length': user_post_length,
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following
     }
     
     return render(request, 'profile.html', context)
@@ -58,6 +88,11 @@ def follow(request):
         if FollowersCount.objects.filter(follower=follower, user=user).first():
             delete_follower = FollowersCount.objects.get(follower=follower, user=user)
             delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
         
     else:
         return redirect('/')
@@ -150,15 +185,15 @@ def signin(request):
         
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            return redirect('index')
         else:
             messages.info(request, 'Credential Invalid')
             return redirect('signin')
     else:
         return render(request, 'signin.html')
 
-@login_required(login_url='signin')        
+@login_required(login_url='/')        
 def logout(request):
      auth.logout(request)
-     return redirect('signin')
+     return redirect('/')
 
